847294b8f3e8b13ccf54058b07e3dbed578a21e2
wb-wll359241 <wb-wll359241@alibaba-inc.com>
2021-04-22 17:14:24 +0800

    develop ts demo

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o():"function"==typeof define&&define.amd?define([],o):"object"==typeof exports?exports.yui=o():e.yui=o()}(self,(function(){return console.log("hi"),{}}));
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+module.exports = {
+  // 1. 影响提示, 2. 文件大小 development/production
+  // mode: " local, dev, production",
+  // 入口是 tsx, 但程序不认识 jsx, 配置 rules
+  entry: {
+    index: "./lib/index.tsx"
+  },
+  // 输出目录
+  output: {
+    // 因为不同的操作系统, 路径不一样, 所以使用 __dirname, 当前路径
+    path: path.resolve(__dirname, "dist/lib"),
+    // 库的name
+    library: "YUI",
+    // 库最后导出的格式
+    libraryTarget: "umd"
+  },
+
+  // 配置 import 引入, 尝试按顺序解析这些后缀名,如果有多个文件有相同的名字，但后缀名不同，webpack 会解析列在数组首位的后缀的文件 并跳过其余的后缀
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      // 配置 ts tsx
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
+      },
+    ]
+  }
+};
